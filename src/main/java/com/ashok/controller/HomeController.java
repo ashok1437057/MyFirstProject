2ea60657b2e2c8a3f7b42bb13c8bd0b674802754
ashok1437057 <badamashok.404@gmail.com>
2021-07-07 23:36:25 +0530

    second commit

--- src/main/java/com/ashok/controller/HomeController.java
+++ src/main/java/com/ashok/controller/HomeController.java
@@ -8,6 +8,11 @@ public class HomeController {
 	@GetMapping("/hello")
 	public String welcome() {
 		return "Hi ashok welcome to spring boot";
+		
 	}
-
+	@GetMapping("/sayHi")
+	public String welcome2() {
+		return "Hi ashok welcome to java development environment";
+		
+	}
 }
