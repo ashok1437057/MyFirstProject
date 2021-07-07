e4947afa66d4278ba4446439ab1b0686d8713c7a
ashok1437057 <badamashok.404@gmail.com>
2021-07-07 23:14:45 +0530

    first commit

---
+++ src/test/java/com/ashok/MyFirstProjectApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ashok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
